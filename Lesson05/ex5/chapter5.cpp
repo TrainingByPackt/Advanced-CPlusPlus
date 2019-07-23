98d15778444a99175bf348cbc657128d2fe6e52a
olena-lizina <31206094+olena-lizina@users.noreply.github.com>
2019-07-23 17:44:35 +0300

    Update chapter5.cpp
    Changed the callable object to another

--- Lesson05/ex5/chapter5.cpp
+++ Lesson05/ex5/chapter5.cpp
@@ -5,63 +5,67 @@
 //============================================================================
 
 #include <iostream>
-#include <thread>
 #include <future>
 
-class CalcSum
+class ToUppercase
 {
 	public:
-	int operator()(int from, int to)
+	std::string operator()(const std::string& bufIn)
 	{
-		long long int sum{0};
-		for (int i= from; i < to; ++i )
+		std::string bufferOut;
+		for (std::string::const_iterator iter = bufIn.begin(); iter != bufIn.end(); iter++)
 		{
-			sum += i;
+			if (*iter >= 97 && *iter <= 122)
+			{
+				bufferOut += static_cast<char>(static_cast<int>(*iter) - 32);
+			}
+			else
+			{
+				bufferOut += *iter;
+			}
 		}
-		return sum;
+		return bufferOut;
 	}
 
-	void operator()(int from, int to, std::promise<int> result)
+	void operator()(const std::string& bufIn, std::promise<std::string> result)
 	{
-		long long int sum{0};
-		for (int i= from; i < to; ++i )
+		std::string bufferOut;
+		for (std::string::const_iterator iter = bufIn.begin(); iter != bufIn.end(); iter++)
 		{
-			sum += i;
+			if (*iter >= 97 && *iter <= 122)
+			{
+				bufferOut += static_cast<char>(static_cast<int>(*iter) - 32);
+			}
+			else
+			{
+				bufferOut += *iter;
+			}
 		}
-		result.set_value(sum);
+		result.set_value(bufferOut);
 	}
 };
 
 int main()
 {
-	int from = 0;
-	int to = 500;
-	int step = 300;
-
-	CalcSum calcSum1;
-	std::packaged_task<int(int,int)> sumTask1(calcSum1);
-	std::future<int> sumResult1= sumTask1.get_future();
-	std::thread thr1(std::move(sumTask1), from, to);
+	ToUppercase ptConverter;
+	std::packaged_task<std::string(const std::string&)> upperCaseResult1(ptConverter);
+	std::future<std::string> futureUpperResult1= upperCaseResult1.get_future();
+	std::thread thr1(std::move(upperCaseResult1), "This is a string for the packaged task task");
 	thr1.detach();
 
-	from = to;
-	to += step;
+	ToUppercase fConverter;
+	std::future<std::string> futureUpperResult2 = std::async(fConverter, "This is a string for the asynchronous task");
 
-	CalcSum calcSum2;
-	std::future<int> sumResult2 = std::async(calcSum2, from, to);
-
-	from = to;
-	to += step;
-
-	CalcSum calcSum3;
-	std::promise<int> sumRes;
-	std::future<int> sumResult3 = sumRes.get_future();
-	std::thread thr2(calcSum3, to, from, std::move(sumRes));
+	ToUppercase pConverter;
+	std::promise<std::string> promiseResult;
+	std::future<std::string> futureUpperResult3 = promiseResult.get_future();
+	std::thread thr2(pConverter, "This is a string for the task that returns a promise", std::move(promiseResult));
 	thr2.detach();
 
-	sumResult3.wait();
-	auto sum = sumResult1.get() + sumResult2.get() + sumResult3.get();
-	std::cout << "sum = " << sum << std::endl;
-
-	return 0;
+	futureUpperResult3.wait();
+	std::cout << "Converted strings: "
+			<< futureUpperResult1.get() << std::endl
+			<< futureUpperResult2.get() << std::endl
+			<< futureUpperResult3.get() << std::endl;
+    return 0;
 }
